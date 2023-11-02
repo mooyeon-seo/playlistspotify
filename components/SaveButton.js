b9707bb6cf36b405c5bb6adb74453a66eb5092fe
Mooyeon Seo <mooyeon.seo@raspberrysherbet.ca>
2023-11-01 20:01:37 -0400

    complete buttons UI

---
+++ components/SaveButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default SaveButton() {
+    return (
+        <button>Save To Spotify</button>
+    );
+};
---
+++ components/SearchButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default SearchButton() {
+    return (
+        <button>
+            Search
+        </button>
+    );
+}
